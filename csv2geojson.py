a05bbc4944a404d39ce682aca9518c728a6af1eb
Alessandro Cifani <alessandro.cifani@gmail.com>
2016-12-09 17:22:36 +0100

    Arg parsing

--- csv2geojson.py
+++ csv2geojson.py
@@ -1,6 +1,7 @@
 import sys
+import os
 import csv
-import getopt
+import argparse
 
 
 def read_src_file(src_file_path):
@@ -15,12 +16,24 @@ def write_dest_file(dest_file_path):
     pass
 
 
-def usage():
-    pass
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--src', required=True,
+                        help='Source CSV file to read from')
+    parser.add_argument('--dest', required=True,
+                        help='Destination GeoJSON file to write to')
+    parser.add_argument('--excel', default=False, action='store_true',
+                        help='Use for CSV files with Excel format')
+    opts = parser.parse_args()
+    if not (opts.src and opts.dest):
+        parser.error("You have to specify both source and destination file.")
+
+    return opts
 
 
 def main(argv):
     # get args
+    opts = parse_args()
 
     # read csv file
 
@@ -30,4 +43,4 @@ def main(argv):
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
