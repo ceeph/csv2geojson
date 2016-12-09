457e58b7cc4bcdd63c0b4774059e2ba7c4b96551
Alessandro Cifani <alessandro.cifani@gmail.com>
2016-12-09 16:33:32 +0100

    Functions and main skeleton

---
+++ csv2geojson.py
@@ -0,0 +1,33 @@
+import sys
+import csv
+import getopt
+
+
+def read_src_file(src_file_path):
+    pass
+
+
+def format_to_geojson():
+    pass
+
+
+def write_dest_file(dest_file_path):
+    pass
+
+
+def usage():
+    pass
+
+
+def main(argv):
+    # get args
+
+    # read csv file
+
+    # format file to geoJSON
+
+    # write dest file
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
